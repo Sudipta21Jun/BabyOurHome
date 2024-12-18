85178b61269db04bf3828b40a16e7f6bc67448dd
Sudipta21Jun <Sudipta.satyam.18@gmail.com>
2024-12-18 21:57:11 +0530

    Add files via upload
    initiate fake window first

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let windowVisible = false;
+
+function toggleWindow() {
+    if (windowVisible) {
+        document.getElementById('realWindow').classList.remove('hidden');
+        document.getElementById('prank').classList.add('hidden');
+    } else {
+        document.getElementById('prank').classList.remove('hidden');
+        document.getElementById('realWindow').classList.add('hidden');
+    }
+    windowVisible = !windowVisible;
+}
+
+function showSurprise() {
+    document.getElementById('specialMessage').classList.remove('hidden');
+    const music = document.getElementById('music');
+    music.play();
+}
